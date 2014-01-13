09c52c6a99bce4f7316c299ed9693976f35a77e2
Pamela Holmes <phfullsail@fullsail.edu>
2014-01-12 21:28:47 -0600

    Screencast: Using Arrays in Expressions

--- variables/variable/js/script.js
+++ variables/variable/js/script.js
@@ -1,17 +1,8 @@
-// Expressions - Arithemetic Operators
+// Expressions with arrays
+
+var orangeBins =[234, 567, 883];
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
 
 
-var quiz1 = 87;
-var quiz2 = 100;
-var quiz3 = 60;
-var quiz4 = 80;
 
-// the average is the sume of hte items divided by the number of items
-
-var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
-//console.log(average);
-
-var length = 7;
-var width = 6;
-var perimeter = length * 2 + width *2;
-console.log(perimeter);
+console.log(total);
