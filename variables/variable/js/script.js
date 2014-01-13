38e5ddb3cc510049c2ef51c76a3f40c9ffdf7498
Pamela Holmes <phfullsail@fullsail.edu>
2014-01-12 22:02:52 -0600

    Screencast: Assignment Operators

--- variables/variable/js/script.js
+++ variables/variable/js/script.js
@@ -1,12 +1,7 @@
-// Casting
+// Assignment Operators
 
-/*var stringVar = "6";
-var result = 7 + Number(stringVar);*/
-
-var areaCode = 407;
-var firstPart = 203;
-var secPart = 4529;
-
-var phoneNo = "(" + String(areaCode)  + ")" + String(firstPart) + " - " + String(secPart);
-
-console.log(phoneNo);
+var a = 3;
+//a += 4; // a = a + 4
+//a++;
+a--;
+console.log(a);
