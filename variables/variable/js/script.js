3af6f0916b7b61dec1231539e223a6753d615fa2
Pamela Holmes <phfullsail@fullsail.edu>
2014-01-12 20:33:40 -0600

    Example Expressions screencast

--- variables/variable/js/script.js
+++ variables/variable/js/script.js
@@ -1,40 +1,6 @@
 // Javascript
 
-var a = 2;
+var yearBorn = 1967
+var age = 2014 - yearBorn - 1;
 
-a = a + 3;
-
-console.log(a);
-
-var yearBorn = 1955;
-var age; //declaration
-age = 46; //definition
-// storage container = value
-
-var yearOfBirth = 1967;
-
-var isStudent = false;
-
-var kermit = "my friend";
-
-var firstName = "Pamela";
-
-var phrase = 'I don\'t know \n You\'ll have to speak to the professor';
-
-//alert(phrase);
-
-var muppetNames = ["kermit", "miss piggy", firstName];
-
-console.log(muppetNames[2]);
-
-muppetNames[1] = "rizzo";
-
-console.log(muppetNames[1]);
-
-
-
-/*console.log("Hello World");
-console.log(yearBorn);
-*/
-
-alert("Hello World");
+console.log(age);
