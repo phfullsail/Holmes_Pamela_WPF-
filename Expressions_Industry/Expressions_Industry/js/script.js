95002c649e0ac6e24736d622907c7673617a1997
Pamela Holmes <phfullsail@fullsail.edu>
2014-01-14 21:35:21 -0600

    Assignment: Expressions - Industry

--- Expressions_Industry/Expressions_Industry/js/script.js
+++ Expressions_Industry/Expressions_Industry/js/script.js
@@ -1,13 +1,4 @@
-// Pamela Holmes - 01/14/2014 - Expressions_Personal
+// Pamela Holmes - 01/14/2014 - Expressions_Industry
 
-// Let's calculate the interest that you are paying on your car
+// Converting page dimensions from inches to pixels
 
-var loanAmount = prompt("Let's figure out how much that new car is really costing you. \nPlease enter your loan amount");
-var interestRate = prompt("Enter your interest rate");
-var loanLength = prompt("Please enter the loan term in months");
-
-var interestPaid = loanAmount * (interestRate/100) * loanLength;
-
-var result = alert("You will pay "+ "$"+ interestPaid + " " + " in interest if you go the full term of the loan.");
-
-console.log(interestPaid); 
