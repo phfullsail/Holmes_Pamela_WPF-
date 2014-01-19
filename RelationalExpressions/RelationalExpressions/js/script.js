babbf95a640cc0049a342827b31475cb92358195
Pamela Holmes <phfullsail@fullsail.edu>
2014-01-19 14:39:50 -0600

    Relational Expressions

---
+++ RelationalExpressions/RelationalExpressions/js/script.js
@@ -0,0 +1,18 @@
+// Pamela Holmes - 01/19/2014 - Relational Expressions
+
+var kidHeight = 30;
+var minHeight = 48;
+
+//if the child is old enough print to the console "you can ride!"
+//if kid over 48 inches in height
+if(kidHeight > minHeight) {
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+}
+
+
+
+
+
+
+
