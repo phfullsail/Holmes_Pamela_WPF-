978a0de7a2a44dd73bed56f9f8b96e0205228e1b
Pamela Holmes <phfullsail@fullsail.edu>
2014-01-19 14:58:38 -0600

    If and Else

---
+++ IfAndElse/.metadata/.plugins/org.eclipse.core.resources/.history/29/60c789054c8100131e26934dfdfc3f96
@@ -0,0 +1,19 @@
+// Pamela Holmes - 01/19/2014 - With an Expression
+
+var kidHeight = 47;
+var minHeight = 48;
+var sneakerLift = 2;
+
+//if the child is old enough print to the console "you can ride!"
+//if kid over 48 inches in height
+if(kidHeight + sneakerLift > minHeight) {
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+}
+
+
+
+
+
+
+
---
+++ IfAndElse/IfAndElse/js/script.js
@@ -0,0 +1,18 @@
+// Pamela Holmes - 01/19/2014 - If and Else
+var kidHeight = 48;
+var minHeight = 48;
+
+//if the child is old enough print to the console "you can ride!"
+//if kid over 48 inches in height
+if(kidHeight > minHeight) {
+	//code performed if condition is true
+	console.log("You can ride the coaster!");
+}else{
+	//code performed if condition is false
+	console.log("Sorry kid, you've got some growing to do first!");
+}
+
+
+
+
+
