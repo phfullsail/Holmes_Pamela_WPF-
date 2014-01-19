a286601145c183f483a41d30ce44ec402ee4cbe0
Pamela Holmes <phfullsail@fullsail.edu>
2014-01-19 16:05:48 -0600

    Ternaries

---
+++ Ternaries/Ternaries/js/script.js
@@ -0,0 +1,26 @@
+// Conditional Logic - Ternary Operators
+
+
+
+//var gpa = 48;
+
+//if gpa over min 2.0 score, the student can graduate
+
+/*if(gpa > 2.0){
+	console.log("You can graduate!");
+}else {
+	console.log("GPA is too low!");
+}*/
+
+//(gpa > 2.0) ? console.log("You can graduate!") : console.log("GPA is too low!");
+
+
+var age = 11;
+var book;
+
+//if child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
+
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
+
+
