5ff0b979c969680e58a3815281969b1aa54bf05a
Pamela Holmes <phfullsail@fullsail.edu>
2014-01-27 19:23:08 -0600

    Function Invocation

---
+++ FunctionInvocation/FunctionInvocation/js/script.js
@@ -0,0 +1,18 @@
+// Pamela Holmes - 01/27/2014 - Basic Invocation
+
+
+function outptMsg (argument) {
+  console.log("Hello World");
+}
+
+function calcArea() {
+	var width = 20;
+	var height = 30;
+	var area = width * height;
+	console.log(area);
+}
+
+
+calcArea();
+calcArea();
+calcArea();
