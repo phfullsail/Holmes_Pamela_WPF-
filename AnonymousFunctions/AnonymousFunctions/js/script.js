9a599f30fa5c95e8ac8f9872f725ad6a47bb60ed
Pamela Holmes <phfullsail@fullsail.edu>
2014-01-28 07:01:10 -0600

    Anonymous Functions

---
+++ AnonymousFunctions/AnonymousFunctions/js/script.js
@@ -0,0 +1,15 @@
+// Pamela Holmes - 01/28/2014 - Anonymous Functions
+
+
+
+var calcArea = function(width, height) { // defining
+	//code the function runs
+	var area = width * height;
+	return area;
+};
+
+var a = calcArea(20, 30); // invoking
+
+console.log(a);
+
+
