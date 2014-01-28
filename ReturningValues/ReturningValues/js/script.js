62de91b767a64569b04b877cd836fe3abd413c39
Pamela Holmes <phfullsail@fullsail.edu>
2014-01-27 19:56:11 -0600

    Returning Values

---
+++ ReturningValues/ReturningValues/js/script.js
@@ -0,0 +1,10 @@
+// Pamela Holmes - 01/27/2014 - Function returning Values
+
+var total = calcArea(30, 20);
+
+function calcArea(w, h) {
+	var area = w * h;
+	return area; // function spitting out
+}
+
+console.log(total);
