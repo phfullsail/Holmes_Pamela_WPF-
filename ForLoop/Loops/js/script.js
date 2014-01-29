9abb6f44a9fd6335d8307b90d85f74c4c34a6e14
Pamela Holmes <phfullsail@fullsail.edu>
2014-01-28 22:09:37 -0600

    ForLoops

---
+++ ForLoop/Loops/js/script.js
@@ -0,0 +1,23 @@
+// Pamela Holmes - 01/27/2014 - For Loops
+
+
+var b = 10;// sets up the index
+
+while (b > 0) {// checks the condition
+	console.log(b + " kegs on the wall");
+	b--;// increments or decrements the index
+}
+
+var c = 10;
+
+do{
+	console.log(c + " kegs on the wall");
+	c--;
+}
+while(c >0);
+
+
+
+for (var i = 10; i > 0; i--) {
+	console.log(i + " kegs on the wall");
+}
