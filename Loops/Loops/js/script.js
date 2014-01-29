0950956356f2655f12564fef458ec719f4257108
Pamela Holmes <phfullsail@fullsail.edu>
2014-01-28 21:53:06 -0600

    Loops

---
+++ Loops/Loops/js/script.js
@@ -0,0 +1,10 @@
+// Pamela Holmes - 01/27/2014 - Loops
+
+
+var b = 50;// sets up the index
+
+while (b > 0) {// checks the condition
+	console.log(b + " kegs on the wall");
+	b--;// increments or decrements the index
+}
+
