a177e30b03fe799c3b0e187586324249074edc22
Pamela Holmes <phfullsail@fullsail.edu>
2014-01-28 22:00:35 -0600

    DoWhile Loop

---
+++ DoWhileLoop/Loops/js/script.js
@@ -0,0 +1,17 @@
+// Pamela Holmes - 01/27/2014 - Do While Loops
+
+
+var b = 10;// sets up the index
+
+while (b > 0) {// checks the condition
+	console.log(b + " kegs on the wall");
+	b--;// increments or decrements the index
+}
+
+var c = 10;
+
+do{
+	console.log(c + " kegs on the wall");
+	c--;
+}
+while(c >0);
